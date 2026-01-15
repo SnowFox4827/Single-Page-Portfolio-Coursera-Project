2591afe3b54893021fba857c491ffd3f3652f917
SnowFox4827 <dwplocek@outlook.com>
2026-01-15 13:53:39 -0600

    Update script.js
    Finished Project

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ function addRecommendation() {
     element.setAttribute("class","recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
     // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
+    document.getElementById("all_recommendations").appendChild(element);
+
+    // Toggle confirmation popup
+    showPopup(true);
     
     // Reset the value of the textarea
     recommendation.value = "";
